5833134099fede4ab6143a34bbf49df8aa7add1b
ducnv2509 <ducit2509@gmail.com>
2022-09-19 15:48:54 +0700

    temp dis token

--- routers/AlertRouter.js
+++ routers/AlertRouter.js
@@ -5,26 +5,26 @@ import myLogger from '../winstonLog/winston.js';
 const router = express.Router();
 
 
-router.get('/getAllAlert', validateTokenStaffAccess, async (req, res, next) => {
+router.get('/getAllAlert' , async (req, res, next) => {
     let { start_day, end_day, tenant, limit, page } = req.body;
     let response = await getAllAlert(start_day, end_day, tenant, limit, page);
     next(response);
 })
 
-router.get('/:id', validateTokenStaffAccess, async (req, res, next) => {
+router.get('/:id',  async (req, res, next) => {
     let { id } = req.params;
     let response = await getDetailAlert(id);
     next(response);
 })
 
 
-router.get('/', validateTokenStaffAccess, async (req, res, next) => {
+router.get('/', async (req, res, next) => {
     let { query, limit, sort, page } = req.query;
     let response = await getAlert(query, limit, sort, page);
     next(response);
 })
 
-router.put('/:id/', validateTokenStaffAccess, async (req, res, next) => {
+router.put('/:id/', async (req, res, next) => {
     myLogger.info("in ---------------")
     let { id } = req.params;
     let response = undefined;
@@ -32,7 +32,7 @@ router.put('/:id/', validateTokenStaffAccess, async (req, res, next) => {
     next(response);
 })
 
-router.post('/create/:id', validateTokenStaffAccess, async (req, res, next) => {
+router.post('/create/:id', async (req, res, next) => {
     let { id } = req.params;
     let { severity, owners } = req.body;
     let { email } = req.payload;
